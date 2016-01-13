832ba7ea2ef29665baf00cfd4821870c21302015
Zhiyuan Tang <tangzy@cslt.riit.tsinghua.edu.cn>
2016-01-13 11:06:33 +0800

    run_mpe.sh, more details

--- egs/wsj/s5/run_mpe.sh
+++ egs/wsj/s5/run_mpe.sh
@@ -10,14 +10,16 @@
 
 chunk_width=75         # for make_denlats.sh
 chunk_left_context=30
-stage=2
-mpe_stage=-4
+
+stage=33  #0
+mpe_stage=-8  #-8
 cmd=run.pl
 
-trans_model=exp/ctc/tri5b_tree/final.mdl
-graphdir=exp/ctc/lstm_h/graph_tg_0.15
-modeldir=exp/ctc/lstm_h
-tri_ali=exp/ctc/tri5b_tree
+trans_model=exp/ctc/tri5b_tree_monophone/final.mdl
+trans_dir=exp/ctc/tri5b_tree_monophone
+tri_ali=$trans_dir
+graphdir=exp/ctc/lstm_i/graph_bd_tgpr_0.15
+modeldir=exp/ctc/lstm_i
 #tri_ali=${modeldir}_ali
 dir=${modeldir}_mpe
 
@@ -34,30 +36,35 @@ if [ $stage -le 0 ]; then
      #final.mdl.nnet3am -> final.mdl.nnet3am.mpe
 fi
 
-if [ $stage -le 1 ]; then
+if [ ! -f  $modeldir/final.mdl.nnet3am ];then
+  echo "replace objective "linear" with "mpe" to get final.mdl.nnet3am.mpe, linked by final.mdl.nnet3am"
+  exit 1;
+fi
+
+if [ $stage -le 11 ]; then
  echo "make alimgents"
  # refer to modeldir/final.mdl.nnet3am (or final.mdl)
-   steps/nnet3/ctc/align.sh --nj 8 --cmd "$decode_cmd" \
+   steps/nnet3/ctc/align.sh --nj 30 --cmd "$decode_cmd" \
      data/train data/lang_ctc $modeldir ${modeldir}_ali || exit 1;
 fi
 
-if [ $stage -le 2 ]; then
+if [ $stage -le 22 ]; then
  echo "make lattices"
 #   steps/nnet3/ctc/make_denlats.sh  --nj 8 --cmd "$decode_cmd" \
 #     --frames-per-chunk $chunk_width --extra-left-context $chunk_left_context \
 #      $graphdir data/train $modeldir ${modeldir}_denlats || exit 1;
-
-   graphdir_tree=exp/ctc/tri5b_tree/graph_test_tg
+   graphdir_tree=$trans_dir/graph_lang_test_bd_tgpr
+   utils/mkgraph.sh data/lang_test_bd_tgpr $trans_dir $graphdir_tree 
    #modeldir_tree=exp/ctc/tri5b_tree
-   steps/nnet3/make_denlats.sh  --nj 8 --cmd "$decode_cmd" \
+   steps/nnet3/make_denlats.sh  --nj 30 --cmd "$decode_cmd" \
       $graphdir_tree data/train $modeldir ${modeldir}_denlats || exit 1;
 fi
 
-if [ $stage -le 3 ]; then
+if [ $stage -le 33 ]; then
 
  echo "mpe training"
    steps/nnet3/ctc/train_discriminative.sh --cmd "$cuda_cmd" --learning-rate 0.000005 \
-     --num-jobs-nnet 4 --stage $mpe_stage \
+     --num-jobs-nnet 4 --stage $mpe_stage --num-pdfs 193 \
      data/train data/lang ${modeldir}_ali ${modeldir}_denlats \
      $modeldir/final.mdl.nnet3am $dir || exit 1;
 
